24a57ed1be1ed38bb4819e22debeb6c7cbe20a6c
Pranav Kavitkar <pranavkavitkar15@gmail.com>
2025-07-28 01:24:08 +0530

    Update instruction_mem.v

--- instruction_mem.v
+++ instruction_mem.v
@@ -1,60 +1,41 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 24.07.2025 22:03:51
-// Design Name: 
-// Module Name: instruction_mem
-//Instruction memory 16 size each 19 bit wide
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module instruction_memory (
-input clk,
-input reset,
-input [3:0] pc,
-output reg [18:0] instruction_code
-    );
-    
-   reg [18:0] memory [0:15]; 
-    always@(posedge clk or posedge reset)
-    begin
-    if(reset) begin
-    memory[0]  <= 19'b0000000000000000000;
-    memory[1]  <= 19'b0000_0010_0001_0011_001; // ADD
-    memory[2]  <= 19'b0000_0001_0011_0100_001; // SUB
-    memory[3]  <= 19'b0010_0010_0001_0011_001; // MUL
-    memory[4]  <= 19'b0011_0010_0001_0011_001; // DIV
-    memory[5]  <= 19'b0000_0001_0001_0010_010; // INC
-    memory[6]  <= 19'b0001_1111_0001_0010_010; // DEC
-    memory[7]  <= 19'b0100_0010_0001_0011_001; // AND
-    memory[8]  <= 19'b0101_0010_0001_0011_001; // OR
-    memory[9]  <= 19'b0110_0010_0001_0011_001; // XOR
-    memory[10] <= 19'b0111_0000_0001_0010_001; // NOT
-    memory[11] <= 19'b0000_0100_0000_0000_101; // JMP
-    memory[12] <= 19'b0000_0010_0001_0010_100; // BEQ
-    memory[13] <= 19'b0001_0010_0001_0011_100; // BNE
-    memory[14] <= 19'b0000_1010_0000_0000_101; // CALL
-    memory[15] <= 19'b0001_0000_0000_0000_101; // RET
-    
-    
+    input             clk,
+    input             reset,
+    input             write_enable,
+    input      [18:0] instruction_in,
+    input      [3:0]  pc,
+    input             flush_jump,
+    output reg [18:0] instruction_code
+);
+
+    reg [18:0] memory [0:15];
+    reg [3:0]  write_ptr;
+
+    integer i;
+
+    always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            write_ptr <= 0;
+            instruction_code <= 0;
+       
+            for (i = 0; i < 16; i = i + 1) begin
+              
+                    memory[i] <= 19'd0;
+            end
+        end 
+        else if (flush_jump) begin
+            memory[pc] <= 19'd0;  
+        end
+        else if (write_enable) begin
+            memory[write_ptr] <= instruction_in;
+            write_ptr <= (write_ptr == 4'd15) ? 4'd0 : write_ptr + 4'd1;
+        end
     end
-    
+
+    always @(*) begin
+        instruction_code = memory[pc - 1]; 
     end
-    
-    always@(*)begin
-    instruction_code=memory[pc];
-    end
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
 endmodule
